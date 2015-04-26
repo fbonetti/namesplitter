3645f75b1372cbbd4875bcc1e5d3ddb3d00dcc16
Frank Bonetti <frank.r.bonetti@gmail.com>
2015-04-26 13:31:55 -0500

    Hopefully fixed the component styling issue

--- assets/vendor.js
+++ assets/vendor.js
@@ -1,6 +1,6 @@
 
 
-Ember.COMPONENT_CSS_LOOKUP = {"file-drop":"file-drop-8370a3a3"};
+Ember.COMPONENT_CSS_LOOKUP = {"file-drop":"file-drop-c0f88e3a"};
 Ember.ComponentLookup.reopen({
   lookupFactory: function(name, container) {
     var Component = this._super(name, container);
