b07104a2607027a9eec97e47ed11d2a1074cb44b
Frank Bonetti <frank.r.bonetti@gmail.com>
2015-04-26 13:09:49 -0500

    Fixed component styling issue

---
+++ assets/vendor.js
@@ -0,0 +1,12 @@
+
+
+Ember.COMPONENT_CSS_LOOKUP = {"file-drop":"file-drop-8370a3a3"};
+Ember.ComponentLookup.reopen({
+  lookupFactory: function(name, container) {
+    var Component = this._super(name, container);
+    if (!Component) { return; }
+    return Component.reopen({
+      classNames: [Ember.COMPONENT_CSS_LOOKUP[name]]
+    });
+  }
+});
